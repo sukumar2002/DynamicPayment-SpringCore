94124b79a9ba3695dca99b78735563e8992bc184
sukumar2002 <127292893+sukumar2002@users.noreply.github.com>
2025-04-05 23:35:02 +0530

    Update PaymentType.java

--- src/main/java/com/Payment/core/PaymentType.java
+++ src/main/java/com/Payment/core/PaymentType.java
@@ -1,15 +1,14 @@
 package com.Payment.core;
 
-
 public enum PaymentType {
     CREDIT_CARD("creditCard"),
     DEBIT_CARD("debitCard"),
     UPI("upi"),
     NET_BANKING("netBanking"),
     WALLET("wallet");
-
+    
     private final String type;
-
+    
     PaymentType(String type) {
         this.type = type;
     }
